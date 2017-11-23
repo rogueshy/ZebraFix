8551d400b02506d3ed4c9e3f02050b00f698b3b6
RogueShy <rogueshy@derpymail.org>
2017-11-23 12:30:32 +0300

    First working prototype. I can get some security descriptors, but not sure if this is correct values, need more testing.

--- ZebraFix/Program.cs
+++ ZebraFix/Program.cs
@@ -1,15 +1,58 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Runtime.InteropServices;
 
 namespace ZebraFix
 {
     class Program
     {
-        static void Main(string[] args)
-        {
+        static void Main() {
+            IntPtr hPrinter = IntPtr.Zero;
+            Win32Spool.PRINTER_DEFAULTS printerDefaults = new Win32Spool.PRINTER_DEFAULTS();
+            Win32Spool.PRINTER_INFO_3 printerInfo = new Win32Spool.PRINTER_INFO_3();
+            int cbNeeded = 0;
+            try
+            {
+                string printerName = "Fax";
+                IntPtr pPrinterInfo = IntPtr.Zero;
+                printerDefaults.pDatatype = IntPtr.Zero;
+                printerDefaults.pDevMode = IntPtr.Zero;
+                printerDefaults.DesiredAccess = Win32Spool.PRINTER_EXECUTE;
+                if (!Win32Spool.OpenPrinter(printerName, out hPrinter, ref printerDefaults))
+                {
+                    throw new Win32Exception(Marshal.GetLastWin32Error());
+                }
+                if (!Win32Spool.GetPrinter(hPrinter, 3, IntPtr.Zero, 0, out cbNeeded))
+                {
+                    int error = Marshal.GetLastWin32Error();
+                    if (error != Win32Spool.ERROR_INSUFFICIENT_BUFFER)
+                    {
+                        throw new Win32Exception(error);
+                    }
+                    pPrinterInfo = Marshal.AllocHGlobal(cbNeeded);
+                    if (!Win32Spool.GetPrinter(hPrinter, 3, pPrinterInfo, cbNeeded, out cbNeeded))
+                    {
+                        throw new Win32Exception(Marshal.GetLastWin32Error());
+                    }
+
+                    printerInfo = (Win32Spool.PRINTER_INFO_3)Marshal.PtrToStructure(pPrinterInfo, typeof(Win32Spool.PRINTER_INFO_3));
+                    Console.WriteLine(printerInfo.pSecurityDescriptor.dacl.ToString());
+
+                }
+            }
+            catch (Exception ex)
+
+            {
+                // Show errors
+                Console.WriteLine(ex.Message);
+            }
+
         }
+
     }
 }
---
+++ ZebraFix/Win32Spool.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace ZebraFix
+{
+    class Win32Spool
+    {
+        // define access values
+        // from https://msdn.microsoft.com/en-us/library/cc244650.aspx
+        public const uint PRINTER_ACCESS_ADMINISTER = 0x00000004; // this one is not sufficient for even reading security descriptors
+        public const uint PRINTER_ACCESS_USE = 0x00000008; // for basic operations
+        public const uint PRINTER_ALL_ACCESS = 0x000F000C; // statement "to perform all administrative tasks and basic printing operations except synchronization" is a false one, you still wouldn't be able to read security info.
+        public const uint PRINTER_EXECUTE = 0x00020008; // we'll be using this, as it's sufficient at least for getting security descriptor; TODO: check for setting it
+        //some errors
+        public const uint ERROR_INSUFFICIENT_BUFFER = 122;
+        public const uint ERROR_IO_PENDING = 997;
+        public const uint ERROR_FILE_NOT_FOUND = 0x80070002;
+        
+        [StructLayout(LayoutKind.Sequential)]
+        public struct PRINTER_DEFAULTS
+        {
+            public IntPtr pDatatype;
+            public IntPtr pDevMode;
+            public uint DesiredAccess;
+        }
+        [StructLayout(LayoutKind.Sequential)]
+        public struct PRINTER_OPTIONS
+        {
+            public uint cbSize;
+            public IntPtr dwFlags;
+        }
+        
+        // INFO_1 is for general Info, simple fields
+        [StructLayout(LayoutKind.Sequential)]
+        public struct PRINTER_INFO_1
+        {
+            public uint Flags;
+            public string pDescription, pName, pComment;
+        }
+/*
+ * TODO: implement DEVMODE
+ *      // INFO_2 is a detailed info, care for buffer size here could extend to 3MB or even more
+        [StructLayout(LayoutKind.Sequential)]
+        public struct PRINTER_INFO_2
+        {
+            public string pServername,pPrinterName,pShareName,pPortName,pComment,pLocation,pSepFile,pPrintProcessor,pDatatype,pParameters;
+            public uint Attributes, Priority, DefaultPriority, StartTime, UntilTime, Status, cJobs, AveragePPM;
+        }
+*/
+        
+        // INFO_3 is for security info only
+        [StructLayout(LayoutKind.Sequential)]
+        public struct PRINTER_INFO_3
+        {
+            public SECURITY_DESCRIPTOR pSecurityDescriptor;
+        }
+
+        // INFO_4 is for minimal general info
+        // Attributes is really behave more like a Bool type but it really returns DWORD. Some workaround maybe?
+        // There's only two values of Attributes: LOCAL and NETWORK though.
+        [StructLayout(LayoutKind.Sequential)]
+        public struct PRINTER_INFO_4
+        {
+            public string pPrinterName;
+            public string pServerName;
+            public uint Attributes;
+        }
+
+        // INFO_5 is for detailed info with more ATTRIBUTE VALUE
+        // TODO: implement INFO_5
+
+        // INFO_6 is for printer status codes
+        [StructLayout(LayoutKind.Sequential)]
+        public struct PRINTER_INFO_6
+        {
+            public uint dwStatus;
+        }
+
+        // INFO_7 is for directory services info
+        [StructLayout(LayoutKind.Sequential)]
+        public struct PRINTER_INFO_7
+        {
+            public IntPtr pszObjectGUID;
+            public uint dwAction;
+        }
+
+        // INFO_8 is for global default settings
+        // TODO: Implement INFO_8 | after DEVMODE as this one needs DEVMODE to function
+
+        // INFO_9 is for user specific settings
+        // TODO: Implement INFO_9 | same with DEVMODE
+
+        [DllImport("winspool.drv", CharSet = CharSet.Auto, SetLastError = true, EntryPoint = "OpenPrinterW")]
+        public static extern bool OpenPrinter(
+            string pPrinterName,
+            out IntPtr phPrinter,
+            ref PRINTER_DEFAULTS pDefault
+        );
+
+        // this was a test, still not sure if I need this
+        // but it works
+        [DllImport("winspool.drv", CharSet = CharSet.Auto, SetLastError = true, EntryPoint = "OpenPrinter2W")]
+        public static extern bool OpenPrinter2(
+            string pPrintername,
+            out IntPtr phPrinter,
+            ref PRINTER_DEFAULTS pDefault,
+            ref PRINTER_OPTIONS pOptions
+            );
+
+/*      TODO: implement SetPrinter(), needs more testing on GetPrinter though;  
+ *      [DllImport("winspool.drv", SetLastError = true)]
+        public static extern bool SetPrinter(
+            IntPtr hPrinter,
+            int Level,
+            ref PRINTER_INFO_# pPrinter,
+            int Command
+        );
+*/
+        [DllImport("winspool.drv", SetLastError = true)]
+        public static extern bool GetPrinter(
+            IntPtr hPrinter,
+            int Level,
+            IntPtr pPrinter,
+            int cbBuf,
+            out int pcbNeeded
+        );
+
+        [DllImport("winspool.drv", SetLastError = true)]
+        public static extern bool ClosePrinter(
+            IntPtr hPrinter
+        );
+
+        // TODO: check for actual values in last 4 fields, this seems pretty odd;
+        // maybe chose other type
+        [StructLayoutAttribute(LayoutKind.Sequential)] 
+        public struct SECURITY_DESCRIPTOR
+        {
+            public byte revision;
+            public byte size;
+            public short control;
+            public IntPtr owner;
+            public IntPtr group;
+            public IntPtr sacl;
+            public IntPtr dacl;
+        }
+
+    }
+}
